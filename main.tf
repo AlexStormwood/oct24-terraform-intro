bda842a5fa0353d9d0c011ee8b7d6e6902bdfb81
Alex <alex.holder@bigfootds.com>
2025-07-23 20:34:56 +1000

    feat: terraform output and random bucket name

--- main.tf
+++ main.tf
@@ -3,5 +3,9 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "demo_bucket_id_alex-was-here" {
-	bucket = "some-cool-unique-bucket-name"
+	bucket = "some-cool-unique-bucket-name-${random_id.bucket_id_random_chunk.hex}"
+}
+
+resource "random_id" "bucket_id_random_chunk" {
+	byte_length = 4
 }
